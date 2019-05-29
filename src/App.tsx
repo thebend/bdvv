d52985715874a2725e6dfd2e008e88306db5f58b
Benjamin Davidson <ben.davidson@gmail.com>
2019-05-28 21:41:29 -0700

    Rewrite intro splash

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,9 @@ import Help from './Help'
 
 // TODO: add logo
 const SPLASH = <section id="splash">
-	<p>Auto-play any number of videos in an optimally arranged grid with simple drag-and-drop.  Videos start half-way in and loop, ensuring immediate, continuous immersion.</p>
+	<p>Auto-play any number of videos or copies of videos in an optimally arranged grid with simple drag-and-drop.  Videos start half-way in and loop, ensuring immediate, continuous immersion, great for scouring surveillance footage, finding the best highlights from your last gaming stream, and more!</p>
 	<p>Find your favourite moments quickly with thumbnail scrubbing and keyboard shortcuts to jump ahead in time 1m (→) or 10% (Shift →), or adjust playback speed (Ctrl →).</p>
 	<p>Use shortcut keys to rapidly (c)opy or (r)emove displays, set (i)n/(o)ut loop points, (f)ullscreen the display, toggle (m)ute, etc.</p>
-	<p>Great for scouring surveillance footage, finding the best highlights from your last gaming stream, and more!</p>
 	{/* <img src="clipart/surveillance.jpg" alt="Surveillance" />
 	<img src="clipart/television.jpg" alt="Television" /> */}
 </section>
