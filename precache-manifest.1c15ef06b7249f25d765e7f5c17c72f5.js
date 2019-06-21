3234590237afd20092653840edbc9cfd2ff863a4
Benjamin Davidson <ben.davidson@gmail.com>
2019-06-20 21:26:50 -0700

    Updates

---
+++ precache-manifest.1c15ef06b7249f25d765e7f5c17c72f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ec585d2b1a886ca4fa67",
+    "url": "/static/js/main.3046caa5.chunk.js"
+  },
+  {
+    "revision": "5390c4edf2d52c8ad7ae",
+    "url": "/static/js/2.2c35b59a.chunk.js"
+  },
+  {
+    "revision": "ec585d2b1a886ca4fa67",
+    "url": "/static/css/main.596a0ab4.chunk.css"
+  },
+  {
+    "revision": "151b096471faaeadc77d24e46a4b25cb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec26657ab64361012f2eccc07e14cd72.js"
+  "/precache-manifest.1c15ef06b7249f25d765e7f5c17c72f5.js"
 );
 
 workbox.clientsClaim();
