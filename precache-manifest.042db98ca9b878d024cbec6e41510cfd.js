ddbe78d1b031f55306c1654383dfe258592d3c99
Benjamin Davidson <ben.davidson@gmail.com>
2019-07-13 13:20:52 -0700

    Updates

---
+++ precache-manifest.042db98ca9b878d024cbec6e41510cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085d287739e14be5034c72cb322df170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfd3e7f44850182ab8d5",
+    "url": "/static/css/main.088b9dde.chunk.css"
+  },
+  {
+    "revision": "275f7048bbf8c133c7e6",
+    "url": "/static/js/2.15d8d3ac.chunk.js"
+  },
+  {
+    "revision": "bfd3e7f44850182ab8d5",
+    "url": "/static/js/main.b9d503d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
